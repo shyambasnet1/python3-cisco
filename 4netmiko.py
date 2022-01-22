2b89090065a82d519b71628bd316af6ab29bf7c3
shyam basnet <61358211+shyambasnet1@users.noreply.github.com>
2022-01-22 12:07:15 -0600

    Update 4netmiko.py

--- 4netmiko.py
+++ 4netmiko.py
@@ -13,7 +13,23 @@ for devices in devices_list:
         'username': 'shyam',
         'password': 'cisco'
     }
-
+    try:
+        net_connect = ConnectHandler(**ios_device)
+    except (AuthenticationException):
+        print ('Authentication failure: ' + ip_address_of_device)
+        continue
+    except (NetMikoTimeoutException):
+        print ('Timeout to device: ' + ip_address_of_device)
+        continue
+    except (EOFError):
+        print ('End of file while attempting device ' + ip_address_of_device)
+        continue
+    except (SSHException):
+        print ('SSH Issue. Are you sure SSH is enabled? ' + ip_address_of_device)
+        continue
+    except Exception as unknown_error:
+        print ('Some other error: ' + str(unknown_error))
+        continue
     net_connect = ConnectHandler(**ios_device)
     output = net_connect.send_config_set(commands_list)
     print (output)
