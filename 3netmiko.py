6e2cd6460643d0fb3bb10d1c5247543eb2bee3ce
shyam basnet <61358211+shyambasnet1@users.noreply.github.com>
2022-01-22 17:04:45 -0600

    Update 3netmiko.py

--- 3netmiko.py
+++ 3netmiko.py
@@ -1,7 +1,5 @@
 from netmiko import ConnectHandler
 
-with open('commands_file') as f:
-    commands_list = f.read().splitlines()
 
 with open('devices_file') as f:
     devices_list = f.read().splitlines()
@@ -10,7 +8,7 @@ for devices in devices_list:
     print ('Connecting to device" ' + devices)
     ip_address_of_device = devices
     ios_device = {
-        'device_type': 'cisco_ios',
+        'device_type': 'juniper',
         'ip': ip_address_of_device,
         'username': 'shyam',
         'password': 'cisco'
